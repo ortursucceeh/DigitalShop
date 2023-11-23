13122c7bd302c685839b290c67360361ef2b8121
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-11-23 19:28:42 +0200

    add: productItem component

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -4,7 +4,7 @@ import { TQueryValidator } from "../lib/validators/query-validator";
 import { Product } from "../payload-types";
 import { trpc } from "../trpc/client";
 import Link from "next/link";
-import ProductList from "./ProductList";
+import ProductItem from "./ProductItem";
 
 interface ProductReelProps {
   title: string;
@@ -68,7 +68,7 @@ const ProductReel = (props: ProductReelProps) => {
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {map.map((product, i) => {
               return (
-                <ProductList
+                <ProductItem
                   key={`${product}${i}`}
                   product={product}
                   index={i}
